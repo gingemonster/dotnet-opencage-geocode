ba8c24cb5e51ea1b497d8aaa2c7f56b888245251
ivan.shelest <ivan.shelest@solarwinds.com>
2018-06-05 19:23:14 +0300

    added type property to Address Component
    added type property to Address Component

--- OpenCage.Geocode/ResponseObjects/AddressComponent.cs
+++ OpenCage.Geocode/ResponseObjects/AddressComponent.cs
@@ -1,25 +1,41 @@
-﻿namespace OpenCage.Geocode
+﻿using System.Runtime.Serialization;
+
+namespace OpenCage.Geocode
 {
+    [DataContract]
     public class AddressComponent
     {
+        [DataMember(Name = "_type")]
+        public string Type { get; set; }
+
+        [DataMember(Name = "Country")]
         public string Country { get; set; }
 
+        [DataMember(Name = "StateDistrict")]
         public string StateDistrict { get; set; }
 
+        [DataMember(Name = "CountryCode")]
         public string CountryCode { get; set; }
 
+        [DataMember(Name = "State")]
         public string State { get; set; }
 
+        [DataMember(Name = "Suburb")]
         public string Suburb { get; set; }
 
+        [DataMember(Name = "City")]
         public string City { get; set; }
 
+        [DataMember(Name = "BusStop")]
         public string BusStop { get; set; }
 
+        [DataMember(Name = "County")]
         public string County { get; set; }
 
+        [DataMember(Name = "Road")]
         public string Road { get; set; }
 
+        [DataMember(Name = "Postcode")]
         public string Postcode { get; set; }
     }
 }
