734b2a35ec3f74a499cbf048e634f184665d9f39
Zukunft Andy <mail@andyzukunft.de>
2023-09-30 11:10:29 +0200

    Add Geocoder.GeocodeAsync() to support async execution;

--- OpenCage.Geocode/Geocoder.cs
+++ OpenCage.Geocode/Geocoder.cs
@@ -92,10 +92,56 @@ namespace OpenCage.Geocode
             {
                 url += "&countrycode=" + countrycode;
             }
+
             AddCommonOptionalParameters(ref url, limit, minConfidence, noAnnotations, noDedupe, noRecord, abbrv, addRequest);
 
-            return this.GetResponse(url);
+            return GetResponse(url);
         }
+
+        /// <summary>
+        /// Forward geocoding given a textual location
+        /// </summary>
+        /// <param name="query">The query string to be geocoded; a placename or lat+long.</param>
+        /// <param name="language">An IETF format language code (such as es for Spanish or pt-BR for Brazilian Portuguese).</param>
+        /// <param name="countrycode">Restricts the results to the specified country or countries. The country code is a two letter code as defined by the ISO 3166-1 Alpha 2 standard.E.g.gb for the United Kingdom</param>
+        /// <param name="bounds">Provides the geocoder with a hint to the region that the query resides in. This value will restrict the possible results to the supplied region. The bounds parameter should be specified as 4 coordinate points forming the south-west and north-east corners of a bounding box.</param>
+        /// <param name="abbrv">When set to true we attempt to abbreviate and shorten the formatted string we return</param>
+        /// <param name="limit">How many results should be returned. Default is 10. Maximum is 100.</param>
+        /// <param name="minConfidence">An integer from 1-10. Only results with at least this confidence will be returned.</param>
+        /// <param name="noAnnotations">When set to true results will not contain annotations.</param>
+        /// <param name="noDedupe">	When set to true results will not be deduplicated.</param>
+        /// <param name="noRecord">When set to true the query contents are not logged. Please use if you have concerns about privacy and want us to have no record of your query.</param>
+        /// <param name="addRequest">When set to true the various request parameters are added to the response for ease of debugging.</param>
+        /// <returns></returns>
+        public async Task<GeocoderResponse> GeocodeAsync(
+            string query,
+            string language = "en",
+            string countrycode = null,
+            Bounds bounds = null,
+            bool abbrv = false,
+            int limit = 10,
+            int minConfidence = 0,
+            bool noAnnotations = false,
+            bool noDedupe = false,
+            bool noRecord = false,
+            bool addRequest = false)
+        {
+            var url = string.Format(Baseurl, WebUtility.UrlEncode(query), this.key, WebUtility.UrlEncode(language));
+
+            if (bounds != null)
+            {
+                url += $"&bounds={bounds.NorthEast.Longitude}%2C{bounds.NorthEast.Latitude}%2C{bounds.SouthWest.Longitude}%2C{bounds.SouthWest.Latitude}";
+            }
+            if (!string.IsNullOrEmpty(countrycode))
+            {
+                url += "&countrycode=" + countrycode;
+            }
+
+            AddCommonOptionalParameters(ref url, limit, minConfidence, noAnnotations, noDedupe, noRecord, abbrv, addRequest);
+
+            return await GetResponseAsync(url);
+        }
+
         /// <summary>
         /// Reverse geocoding given a latitude and longitude
         /// </summary>
